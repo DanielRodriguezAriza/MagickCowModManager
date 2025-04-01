a2a5f0ff8e6d809fa9533964a7279d0682623d11
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 16:50:44 +0200

    Created new LoadException class

---
+++ MagickCowModManager/MagickCowModManager/Core/Exceptions/LoadException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.Exceptions
+{
+    public class LoadException : Exception
+    {
+        public LoadException() : base()
+        { }
+
+        public LoadException(string? message) : base($"Load Error : {message}")
+        { }
+
+        public LoadException(string? message, Exception innerException) : base($"Load Error : {message}", innerException)
+        { }
+    }
+}
