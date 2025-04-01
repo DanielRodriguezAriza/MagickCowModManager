23c98ce084986d4a517acd9343cd5bb6c2ee12ec
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 15:21:10 +0200

    Added basic code logic for enabling mod files

--- MagickCowModManager/MagickCowModManager/Program.cs
+++ MagickCowModManager/MagickCowModManager/Program.cs
@@ -1,5 +1,57 @@
 ﻿namespace MagickCowModManager
 {
+    public class FileHandler
+    {
+        public enum OperationMode
+        {
+            Copy,
+            SymbolicLink,
+            Hardlink,
+        }
+
+        public OperationMode Mode { get; set; }
+
+        public FileHandler()
+        {
+            this.Mode = OperationMode.Copy;
+        }
+
+        public void EnableModFile(string origin, string destination)
+        {
+            switch (this.Mode)
+            {
+                default:
+                case OperationMode.Copy:
+                    EnableModFile_Copy(origin, destination);
+                    break;
+                case OperationMode.SymbolicLink:
+                    EnableModFile_SymbolicLink(origin, destination);
+                    break;
+                case OperationMode.Hardlink:
+                    EnableModFile_HardLink(origin, destination);
+                    break;
+            }
+        }
+
+        private void EnableModFile_Copy(string org, string dst)
+        {
+            File.Copy(org, dst);
+        }
+
+        private void EnableModFile_SymbolicLink(string org, string dst)
+        {
+            File.CreateSymbolicLink(dst, org);
+        }
+
+        private void EnableModFile_HardLink(string org, string dst)
+        {
+            // TODO : Find a way to implement this in both windows and linux.
+            // For windows I already know what DLL and function to load, just need to find the same for linux.
+            // Also add a "not supported" message / exception for any other system.
+            throw new NotImplementedException("Hard Link usage is not implemented yet!");
+        }
+    }
+
     public class Program
     {
         static void Main(string[] args)
