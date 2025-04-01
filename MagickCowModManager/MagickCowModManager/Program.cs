7af79ad5a251df2a22561eff7faf4962c54c9507
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 15:11:41 +0200

    Initial code upload

---
+++ MagickCowModManager/MagickCowModManager/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MagickCowModManager
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("No se ha hecho nada...");
+                return;
+            }
+            File.CreateSymbolicLink(args[0], args[1]);
+            Console.WriteLine("Se ha creado el symlink.");
+        }
+    }
+}
