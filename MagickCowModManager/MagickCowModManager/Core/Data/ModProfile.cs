79f285f1c1e76ce17825640bf794daeda3677584
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 16:11:01 +0200

    Created ModProfile class
    A simple wrapper to load ModProfileData json files

---
+++ MagickCowModManager/MagickCowModManager/Core/Data/ModProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.Data
+{
+    public class ModProfile
+    {
+        private ModProfileData profileData;
+
+        public string Name { get; private set; }
+        public List<string> Mods { get{ return this.profileData.EnabledMods; } }
+
+        public ModProfile(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+
+            this.Name = filePath;
+            this.profileData = JsonSerializer.Deserialize<ModProfileData>(json);
+        }
+    }
+}
