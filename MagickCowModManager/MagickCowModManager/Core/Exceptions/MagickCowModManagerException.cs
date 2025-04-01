d4b4e64beb19513726cd07bc9f114a2e5db95245
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 19:53:42 +0200

    Added base class for all other types of exceptions

---
+++ MagickCowModManager/MagickCowModManager/Core/Exceptions/MagickCowModManagerException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.Exceptions
+{
+    // Base class for all other types of exceptions
+    public class MagickCowModManagerException : Exception
+    {
+        public MagickCowModManagerException() : base()
+        { }
+
+        public MagickCowModManagerException(string? message) : base(message)
+        { }
+
+        public MagickCowModManagerException(string? message, Exception innerException) : base(message, innerException)
+        { }
+    }
+}
