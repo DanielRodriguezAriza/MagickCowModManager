312cb0aebf811ff5841cb4fdc9ac89e78683a451
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 17:33:40 +0200

    Implemented basic logic for ArgParser class

--- MagickCowModManager/MagickCowModManager/Core/Args/ArgParser.cs
+++ MagickCowModManager/MagickCowModManager/Core/Args/ArgParser.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace MagickCowModManager.Core.Args
 {
-    internal class ArgParser
+    public class ArgParser
     {
+        private ModManager modManager; // Reference to the mod manager instance itself.
+        private Command[] commands; // List of known commands.
+
+        public ArgParser(ModManager modManager)
+        {
+            this.modManager = modManager;
+            this.commands = new Command[]
+            {
+                new Command
+                {
+                    ShortCommand = "h",
+                    LongCommand = "help",
+                    Arguments = Array.Empty<string>(),
+                    Description = "Display this help message.",
+                    Function = null
+                }
+            };
+        }
+
+        public void Parse(string[] args)
+        {
+            // TODO : Implement
+        }
     }
 }
