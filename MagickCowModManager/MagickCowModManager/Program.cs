a5ca1b9ed63eb480d3d2a287842d05eb823a4749
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 19:02:31 +0200

    Modified main program to use new arg parser logic

--- MagickCowModManager/MagickCowModManager/Program.cs
+++ MagickCowModManager/MagickCowModManager/Program.cs
@@ -7,30 +7,19 @@ namespace MagickCowModManager
     {
         static void Main(string[] args)
         {
-            /*
-            if (args.Length != 2)
-            {
-                Console.WriteLine("No se ha hecho nada...");
-                return;
-            }
-            File.CreateSymbolicLink(args[0], args[1]);
-            Console.WriteLine("Se ha creado el symlink.");
-            */
-
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Usage : TODO : Implement usage / help message");
-                return;
-            }
-
             try
             {
-                ModManager modManager = new ModManager(args[0]);
-                modManager.ApplyProfile(args[1]);
+                ModManager modManager = new ModManager();
+                modManager.ParseArguments(args);
             }
             catch (LoadException exception)
             {
                 Console.WriteLine($"Load Error : {exception.Message}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Exception : {exception.Message}");
+                Console.WriteLine($"Stack Trace : {exception.StackTrace}");
             }
         }
     }
