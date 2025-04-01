0b444db07c9418fe02caa2ac94458b3b4567f52c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 17:38:52 +0200

    Added new constructor for CommandHandler class

--- MagickCowModManager/MagickCowModManager/Core/Args/CommandHandler.cs
+++ MagickCowModManager/MagickCowModManager/Core/Args/CommandHandler.cs
@@ -8,6 +8,23 @@ namespace MagickCowModManager.Core.Args
 {
     public class CommandHandler
     {
+        public ModManager ModManager { get; set; } // Reference to a mod manager instance.
         public Command[] Commands { get; set; } // List of known commands.
+
+        public CommandHandler(ModManager modManager)
+        {
+            this.ModManager = modManager;
+            this.Commands = new Command[]
+            {
+                new Command
+                {
+                    ShortCommand = "h",
+                    LongCommand = "help",
+                    Arguments = Array.Empty<string>(),
+                    Description = "Display this help message.",
+                    Function = null
+                }
+            };
+        }
     }
 }
