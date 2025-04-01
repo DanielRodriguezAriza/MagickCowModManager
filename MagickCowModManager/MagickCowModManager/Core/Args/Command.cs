ac2d007998a401b07bae208f27cc78399967864d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 17:27:16 +0200

    Implemented Command struct

--- MagickCowModManager/MagickCowModManager/Core/Args/Command.cs
+++ MagickCowModManager/MagickCowModManager/Core/Args/Command.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace MagickCowModManager.Core.Args
 {
-    internal class Command
+    public struct Command
     {
+        public string ShortCommand { get; set; }
+        public string LongCommand { get; set; }
+        public string[] Arguments { get; set; }
+        public string Description { get; set; }
+        public Action<string[], int> Function { get; set; }
     }
 }
