814f908ad3740c83dcad82cb61d1edb3eade6e8c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 15:53:48 +0200

    Added constructors and path vars to ModManager

--- MagickCowModManager/MagickCowModManager/Core/ModManager.cs
+++ MagickCowModManager/MagickCowModManager/Core/ModManager.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MagickCowModManager
+namespace MagickCowModManager.Core
 {
-    internal class ModManager
+    public class ModManager
     {
+        private FileHandler handler;
+
+        public string ModsContentPath { get; set; }
+        public string GameContentPath { get; set; }
+
+        public ModManager()
+        {
+            this.ModsContentPath = "./Mods";
+            this.GameContentPath = "./Content";
+            this.handler = new FileHandler();
+        }
+
+        public ModManager(string gamePath)
+        {
+            this.ModsContentPath = Path.Combine(gamePath, "/Mods");
+            this.GameContentPath = Path.Combine(gamePath, "/Content");
+            this.handler = new FileHandler();
+        }
+
+        public ModManager(string modsPath, string contentPath)
+        {
+            this.ModsContentPath = modsPath;
+            this.GameContentPath = contentPath;
+            this.handler = new FileHandler();
+        }
+
     }
 }
