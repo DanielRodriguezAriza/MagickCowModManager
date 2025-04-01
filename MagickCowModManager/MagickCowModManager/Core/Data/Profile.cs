57b9b3d2e435aede4682373d08e83f5e0443f0ed
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 16:03:13 +0200

    Created Mod Profile class

---
+++ MagickCowModManager/MagickCowModManager/Core/Data/Profile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.Data
+{
+    // Describes a mod profile, with a list of the mods enabled for this given profile.
+    // NOTE : The profile name is given by the string of the file, so no need to store it internally in here within this class / struct.
+    public class Profile
+    {
+        public List<string> EnabledMods { get; set; }
+
+        public Profile()
+        {
+            this.EnabledMods = new List<string>();
+        }
+
+        public Profile(List<string> enabledMods)
+        {
+            this.EnabledMods = new List<string>(enabledMods);
+        }
+
+        public Profile(string[] enabledMods)
+        {
+            this.EnabledMods = new List<string>(enabledMods);
+        }
+    }
+}
