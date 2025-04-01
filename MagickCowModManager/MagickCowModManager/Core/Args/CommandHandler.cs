6a23b5ff5f27d64a02643e870c63d6f92cf4a8a9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 17:36:55 +0200

    Created CommandHandler class

---
+++ MagickCowModManager/MagickCowModManager/Core/Args/CommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.Args
+{
+    public class CommandHandler
+    {
+        public Command[] Commands { get; set; } // List of known commands.
+    }
+}
