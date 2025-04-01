cc974fa1b03c51a3542b9cb0b566f84a8041b6c8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 23:12:27 +0200

    Implemented final cleanup logic

--- MagickCowModManager/MagickCowModManager/Core/FileHandling/CopyFileHandler.cs
+++ MagickCowModManager/MagickCowModManager/Core/FileHandling/CopyFileHandler.cs
@@ -39,39 +39,23 @@ namespace MagickCowModManager.Core.FileHandling
         {
             string originPath = Path.Combine(this.modsContentPath, modName, "Content");
             string destinationPath = this.gameContentPath;
-            ProcessDirectory(originPath, destinationPath);
-        }
 
-        private void ProcessDirectory(string originPath, string destinationPath)
-        {
             DirectoryInfo originInfo = new DirectoryInfo(originPath);
             DirectoryInfo destinationInfo = new DirectoryInfo(destinationPath);
 
             RegisterData(originInfo);
+
+            foreach (var file in this.filesToInstall)
+                Console.WriteLine(file);
+            Console.ReadLine();
+
             GenerateData(originInfo, destinationInfo);
+            CleanUpData(destinationInfo);
         }
 
         private void RegisterData(DirectoryInfo origin)
         {
-            RegisterDirectoryRec("", origin);
-        }
-
-        private void RegisterDirectoryRec(string parent, DirectoryInfo origin)
-        {
-            FileInfo[] files = origin.GetFiles();
-            foreach (var file in files)
-            {
-                var path = Path.Combine(parent, origin.Name);
-                this.filesToInstall.Add(path);
-            }
-
-            DirectoryInfo[] dirs = origin.GetDirectories();
-            foreach (var dir in dirs)
-            {
-                var path = Path.Combine(parent, dir.Name);
-                this.directoriesToInstall.Add(path);
-                RegisterDirectoryRec(path, dir);
-            }
+            FileSystemHelper.GetChildPaths(origin, this.directoriesToInstall, this.filesToInstall);
         }
 
         private void GenerateData(DirectoryInfo origin, DirectoryInfo destination)
@@ -90,105 +74,40 @@ namespace MagickCowModManager.Core.FileHandling
                 string fileToCopy = Path.Combine(origin.FullName, file);
                 string fileToCreate = Path.Combine(destination.FullName, file);
 
-                if (!File.Exists(fileToCreate) || FileContentsAreEqual(fileToCopy, fileToCreate))
+                if (!File.Exists(fileToCreate) || FileSystemHelper.FileContentsAreEqual(fileToCopy, fileToCreate))
                 {
                     File.Copy(fileToCopy, fileToCreate, true);
                 }
             }
         }
 
-        private void ProcessDirectory(DirectoryInfo origin, DirectoryInfo destination)
+        private void CleanUpData(DirectoryInfo destination)
         {
-            // Copy files from origin to destination
-            FileInfo[] childFilesOrigin = origin.GetFiles();
-            foreach (var file in childFilesOrigin)
-            {
-                ProcessFile(file, destination);
-            }
-
-            // Clean up files that are not in the origin directory
-            // THIS IS ACTUALLY WRONG BECAUSE WE ONLY CHECK THE CURRENT MOD DIR, WHAT ABOUT ALL THE OTHER DIRS?? WE DELETE THE WORK WE JUST DID WTF!!!
-            string[] childFileNamesOrigin = origin.GetFiles().Select(file => file.Name).ToArray();
-            string[] childFileNamesDestination = destination.GetFiles().Select(file => file.Name).ToArray();
-            foreach (var fileName in childFileNamesDestination)
-            {
-                if (!childFileNamesOrigin.Contains(fileName))
-                {
-                    File.Delete(Path.Combine(destination.FullName, fileName));
-                }
-            }
-
-            // Copy directories from origin to destination
-            DirectoryInfo[] childDirsOrigin = origin.GetDirectories();
-            foreach (var dir in childDirsOrigin)
-            {
-                var newDir = destination.CreateSubdirectory(dir.Name);
-                ProcessDirectory(dir, newDir);
-            }
+            CleanUpDataRec("", destination);
         }
 
-        private void ProcessFile(FileInfo originFileInfo, DirectoryInfo destinationDirectoryInfo)
+        private void CleanUpDataRec(string parent, DirectoryInfo destination)
         {
-            bool shouldDeleteFile = false;
-            bool shouldCreateFile = false;
-
-            string originFileName = originFileInfo.FullName;
-            string destinationFileName = Path.Combine(destinationDirectoryInfo.FullName, originFileInfo.Name);
-
-            if (File.Exists(destinationFileName))
+            FileInfo[] files = destination.GetFiles();
+            foreach (var file in files)
             {
-                if (!FileContentsAreEqual(originFileName, destinationFileName))
+                string path = Path.Combine(parent, file.Name);
+                if (!this.filesToInstall.Contains(path))
                 {
-                    shouldCreateFile = true;
-                    shouldDeleteFile = true;
-                }
-            }
-            else
-            {
-                shouldCreateFile = true;
-            }
-
-            if (shouldDeleteFile)
-            {
-                // If an outdated file exists on the destination, delete it.
-                File.Delete(destinationFileName);
-            }
-
-            if (shouldCreateFile)
-            {
-                // If the destination doesn't already have installed the origin file, then copy it.
-                Console.WriteLine($"Installing mod file : {originFileName}");
-                File.Copy(originFileName, destinationFileName); // NOTE : Shitty solution, what about heavy files? don't want to copy those... fucking windows I swear...
-            }
-        }
-
-        private bool FileContentsAreEqual(string filePathA, string filePathB)
-        {
-            bool ans = false;
-
-            using (var fileA = File.Open(filePathA, FileMode.Open, FileAccess.Read))
-            using (var fileB = File.Open(filePathB, FileMode.Open, FileAccess.Read))
-            using (var readerA = new BinaryReader(fileA))
-            using (var readerB = new BinaryReader(fileB))
-            {
-                long lengthA = readerA.BaseStream.Length;
-                long lengthB = readerB.BaseStream.Length;
-
-                if (lengthA == lengthB)
-                {
-                    var bytesA = readerA.ReadBytes((int)lengthA);
-                    var bytesB = readerB.ReadBytes((int)lengthB);
-                    if (bytesA.AsSpan().SequenceEqual(bytesB))
-                        ans = true; // Supposedly in C# this is almost as fast as calling memcmp in C, so yeah, probably can't get faster than this without calling the CRT directly.
-
-                    // Btw, yes, I know, i could just say ans = bytesA.AsSpan().SequenceEquals(bytesB).... But not really... because that fails in windows 11
-                    // for some fucking stupid reason! So the "if(...) ans = true;" is a fucking hack that I need to use to work on that piece of trash OS.
-                    // Why? who knows! But hopefully they'll fix that in a future update!
-                    // "When?" you may ask! Who the fuck knows!
+                    File.Delete(file.FullName);
                 }
             }
 
-            return ans;
+            DirectoryInfo[] dirs = destination.GetDirectories();
+            foreach (var dir in dirs)
+            {
+                string path = Path.Combine(parent, dir.Name);
+                if (!this.directoriesToInstall.Contains(path))
+                {
+                    Directory.Delete(dir.FullName);
+                }
+                CleanUpData(dir);
+            }
         }
     }
 }
