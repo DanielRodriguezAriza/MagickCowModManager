088aa9dae04933d53bda70fef1f46f51169469d7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 16:10:45 +0200

    Modified ModProfileData to be a struct

--- MagickCowModManager/MagickCowModManager/Core/Data/ModProfileData.cs
+++ MagickCowModManager/MagickCowModManager/Core/Data/ModProfileData.cs
@@ -8,21 +8,21 @@ namespace MagickCowModManager.Core.Data
 {
     // Describes a mod profile, with a list of the mods enabled for this given profile.
     // NOTE : The profile name is given by the string of the file, so no need to store it internally in here within this class / struct.
-    public class Profile
+    public struct ModProfileData
     {
         public List<string> EnabledMods { get; set; }
 
-        public Profile()
+        public ModProfileData()
         {
             this.EnabledMods = new List<string>();
         }
 
-        public Profile(List<string> enabledMods)
+        public ModProfileData(List<string> enabledMods)
         {
             this.EnabledMods = new List<string>(enabledMods);
         }
 
-        public Profile(string[] enabledMods)
+        public ModProfileData(string[] enabledMods)
         {
             this.EnabledMods = new List<string>(enabledMods);
         }
