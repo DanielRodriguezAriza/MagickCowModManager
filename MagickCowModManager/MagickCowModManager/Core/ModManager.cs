7e1ac0327594e92340d43410bcad76ff953d2462
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-01 16:03:45 +0200

    Added regions and partial mod loading support to ModManager

--- MagickCowModManager/MagickCowModManager/Core/ModManager.cs
+++ MagickCowModManager/MagickCowModManager/Core/ModManager.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MagickCowModManager.Core
 {
+    // TODO : In the future, add some mod profile handling kind of stuff rather than a single mod load order file. For example, have a profile0, profile1, etc... files, each being just a different mod load order file.
     public class ModManager
     {
+        #region Private Variables
+
         private FileHandler handler;
+
+        #endregion
+
+        #region Public Properties
 
         public string ModsContentPath { get; set; }
         public string GameContentPath { get; set; }
+
+        #endregion
+
+        #region Constructors
 
         public ModManager()
         {
@@ -33,6 +45,26 @@ namespace MagickCowModManager.Core
             this.GameContentPath = contentPath;
             this.handler = new FileHandler();
         }
+
+        #endregion
+
+        #region PublicMethods
+
+        public void LoadAllMods()
+        {
+            // TODO : Implement
+        }
+
+        #endregion
+
+        #region PrivateMethods
+
+        private void ReadModLoadOrder(string path)
+        {
+            File.Open(path, FileMode.Open, FileAccess.Read);
+        }
+
+        #endregion
 
     }
 }
