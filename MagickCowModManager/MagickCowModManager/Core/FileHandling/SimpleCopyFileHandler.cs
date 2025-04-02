592e82872f7c8f7d00632a5b60be92202d90f8de
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-02 04:18:57 +0200

    Created SimpleCopyFileHandler class
    The SimpleCopyFileHandler class implements a simpler version of the CopyFileHandler class.
    
    This appears to be faster despite the fact that it does not perform all of the de-duplication logic... fuck...
    
    Well, faster only when it comes to installing large quantities of files. When dealing with multiple smaller mods that add a few files each, the old implementation is pretty fast. But that implementation misserably fails at properly dealing with larger mods, such as full game overhauls that override tons of files, probably due to the string comparisons and look ups being absurdly slow and linear.
    
    With this new implementation, we have a lot of file overwriting, but at least it is surprisingly faster to install large overhaul mods with tons of assets.
    
    Maybe this is the way we should work?
    
    TODO : Consider multi-install setup rather than single install setup for further improvements in performance and greater support for custom executable mods.

---
+++ MagickCowModManager/MagickCowModManager/Core/FileHandling/SimpleCopyFileHandler.cs
@@ -0,0 +1,70 @@
+﻿using MagickCowModManager.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.FileHandling
+{
+    public class SimpleCopyFileHandler
+    {
+        private string modsContentPath;
+        private string gameContentPath;
+        private Profile profile;
+
+        public SimpleCopyFileHandler(Profile profile, string modsContentPath, string gameContentPath)
+        {
+            this.profile = profile;
+            this.modsContentPath = modsContentPath;
+            this.gameContentPath = gameContentPath;
+        }
+
+        public void InstallMods()
+        {
+            DirectoryInfo origin = new DirectoryInfo(this.modsContentPath);
+            DirectoryInfo destination = new DirectoryInfo(this.gameContentPath);
+            Console.WriteLine($"origin path : {origin.FullName}");
+            Console.WriteLine($"destination path : {destination.FullName}");
+
+            string destinationName = destination.FullName;
+            destination.Delete(true);
+
+            Directory.CreateDirectory(destinationName);
+
+            foreach (var modName in this.profile.EnabledMods)
+            {
+                // Install Mod
+                Console.WriteLine($"Installing Mod \"{modName}\"...");
+
+                string modPath = Path.Combine(this.modsContentPath, modName, "Content");
+                DirectoryInfo originInfo = new DirectoryInfo(modPath);
+
+                ProcessDirectoryRec(originInfo, destination);
+            }
+        }
+
+        public void ProcessDirectoryRec(DirectoryInfo origin, DirectoryInfo destination)
+        {
+            foreach (var file in origin.GetFiles())
+            {
+                string targetPath = Path.Combine(destination.FullName, file.Name);
+                if (!File.Exists(targetPath))
+                {
+                    // Console.WriteLine("Creating file : " + targetPath);
+                    File.Copy(file.FullName, targetPath);
+                }
+            }
+
+            foreach (var dir in origin.GetDirectories())
+            {
+                string targetPath = Path.Combine(destination.FullName, dir.Name);
+                if (!Directory.Exists(targetPath))
+                {
+                    Directory.CreateDirectory(targetPath);
+                }
+                ProcessDirectoryRec(dir, new DirectoryInfo(targetPath));
+            }
+        }
+    }
+}
