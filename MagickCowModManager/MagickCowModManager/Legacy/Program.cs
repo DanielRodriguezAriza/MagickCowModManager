ec7b4a8b8a815c8533b8469f3b9f8214724e3d2c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-03 01:37:27 +0200

    Moved main program to Legacy subdirectory

---
+++ MagickCowModManager/MagickCowModManager/Legacy/Program.cs
@@ -0,0 +1,35 @@
+﻿using MagickCowModManager.Legacy.Core;
+using MagickCowModManager.Legacy.Core.Exceptions;
+
+namespace MagickCowModManager.Legacy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ModManager modManager = new ModManager();
+                modManager.ParseArguments(args);
+            }
+            catch (LoadException exception)
+            {
+                Console.WriteLine($"Load Error : {exception.Message}");
+            }
+            catch (ParseException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (CommandException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Unhandled Exception Found!");
+                Console.WriteLine(exception.Message);
+                Console.WriteLine(exception.StackTrace);
+            }
+        }
+    }
+}
