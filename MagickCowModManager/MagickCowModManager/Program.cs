2ade9abcb9d05bcc65953223c8fdecf0e7213006
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-03 15:42:00 +0200

    Updated program to use new function names

--- MagickCowModManager/MagickCowModManager/Program.cs
+++ MagickCowModManager/MagickCowModManager/Program.cs
@@ -9,13 +9,13 @@ namespace MagickCowModManager
             ModManager modManager = new ModManager();
 
             Console.WriteLine("Installs:");
-            foreach (var x in FileSystemUtil.GetChildDirectories(modManager.PathInstalls)) Console.WriteLine($"    - {x}");
+            foreach (var x in FileSystemUtil.GetChildDirectoriesName(modManager.PathInstalls)) Console.WriteLine($"    - {x}");
 
             Console.WriteLine("Mods:");
-            foreach (var x in FileSystemUtil.GetChildDirectories(modManager.PathMods)) Console.WriteLine($"    - {x}");
+            foreach (var x in FileSystemUtil.GetChildDirectoriesName(modManager.PathMods)) Console.WriteLine($"    - {x}");
 
             Console.WriteLine("Profiles:");
-            foreach (var x in FileSystemUtil.GetChildDirectories(modManager.PathProfiles)) Console.WriteLine($"    - {x}");
+            foreach (var x in FileSystemUtil.GetChildDirectoriesName(modManager.PathProfiles)) Console.WriteLine($"    - {x}");
         }
     }
 }
