b7faa82fe0f2c4e8aea5eaf2c22dd9a7747cebb9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-03 04:25:58 +0200

    Created struct to hold ProfileInfo
    The profile info struct will contain the information regarding all of the data about a magicka install profile.
    
    The magicka install profile allows specifying both the base game install and the list of mods to be used on top.

---
+++ MagickCowModManager/MagickCowModManager/Core/ProfileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core
+{
+    public struct ProfileInfo
+    {
+        public string Name { get; set; }
+        public string Install { get; set; }
+        public string[] Mods { get; set; }
+
+        public ProfileInfo()
+        {
+            this.Name = "New Profile";
+            this.Install = "Magicka";
+            this.Mods = Array.Empty<string>();
+        }
+    }
+}
