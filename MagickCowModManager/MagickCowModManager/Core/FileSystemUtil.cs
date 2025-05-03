e1f0f1497dd0f12f496c4d2858f423c929be860d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-03 15:39:21 +0200

    Added static classes and new methods to FileSystemUtil to handle internal file and dir info ops

--- MagickCowModManager/MagickCowModManager/Core/FileSystemUtil.cs
+++ MagickCowModManager/MagickCowModManager/Core/FileSystemUtil.cs
@@ -9,15 +9,36 @@ namespace MagickCowModManager.Core
 {
     public static class FileSystemUtil
     {
-        public static List<string> GetChildDirectories(string path)
+        #region DirInfoOps
+
+        private static class DirInfoOps
         {
-            return GetChildDirectories(new DirectoryInfo(path));
+            public static List<DirectoryInfo> GetChildDirectories(DirectoryInfo directoryInfo)
+            {
+                var ans = new List<DirectoryInfo>(directoryInfo.GetDirectories());
+                return ans;
+            }
         }
 
-        public static List<string> GetChildDirectories(DirectoryInfo directoryInfo)
+        public static List<DirectoryInfo> GetChildDirectoriesInfo(string path)
         {
-            var directories = directoryInfo.GetDirectories();
-            List<string> result = new List<string>(directories.Length);
+            return GetChildDirectoriesInfo(new DirectoryInfo(path));
+        }
+
+        public static List<DirectoryInfo> GetChildDirectoriesInfo(DirectoryInfo directoryInfo)
+        {
+            return DirInfoOps.GetChildDirectories(directoryInfo);
+        }
+
+        public static List<string> GetChildDirectoriesName(string path)
+        {
+            return GetChildDirectoriesName(new DirectoryInfo(path));
+        }
+
+        public static List<string> GetChildDirectoriesName(DirectoryInfo directoryInfo)
+        {
+            var directories = GetChildDirectoriesInfo(directoryInfo);
+            List<string> result = new List<string>(directories.Count);
             foreach (var dir in directories)
             {
                 result.Add(dir.Name);
@@ -25,15 +46,38 @@ namespace MagickCowModManager.Core
             return result;
         }
 
-        public static List<string> GetChildFiles(string path)
+        #endregion
+
+        #region FileInfoOps
+
+        public static class FileInfoOps
         {
-            return GetChildFiles(new DirectoryInfo(path));
+            public static List<FileInfo> GetChildFiles(DirectoryInfo directoryInfo)
+            {
+                var ans = new List<FileInfo>(directoryInfo.GetFiles());
+                return ans;
+            }
         }
 
-        public static List<string> GetChildFiles(DirectoryInfo directoryInfo)
+        public static List<FileInfo> GetChildFilesInfo(string path)
         {
-            var files = directoryInfo.GetFiles();
-            List<string> result = new List<string>(files.Length);
+            return GetChildFilesInfo(new DirectoryInfo(path));
+        }
+
+        public static List<FileInfo> GetChildFilesInfo(DirectoryInfo directoryInfo)
+        {
+            return FileInfoOps.GetChildFiles(directoryInfo);
+        }
+
+        public static List<string> GetChildFilesName(string path)
+        {
+            return GetChildFilesName(new DirectoryInfo(path));
+        }
+
+        public static List<string> GetChildFilesName(DirectoryInfo directoryInfo)
+        {
+            var files = GetChildFilesInfo(directoryInfo);
+            List<string> result = new List<string>(files.Count);
             foreach (var file in files)
             {
                 result.Add(file.Name);
@@ -41,117 +85,128 @@ namespace MagickCowModManager.Core
             return result;
         }
 
-        // Function to "copy" a file from source to destination.
-        // Assume usage of hardlinks by default.
-        // TODO : Improve these shitty comments and clean up the code...
+        #endregion
+
+        #region FileOps
+
         public static void CopyFile(string source, string destination, FileHandlingMode fileHandlingMode = FileHandlingMode.HardLink)
         {
-            CopyFileInternal(source, destination, fileHandlingMode);
+            FileOps.CopyFile(source, destination, fileHandlingMode);
         }
 
-        public static void CopyFileInternal(string source, string destination, FileHandlingMode fileHandlingMode)
+        private static class FileOps
         {
-            switch (fileHandlingMode)
+            public static void CopyFile(string source, string destination, FileHandlingMode fileHandlingMode = FileHandlingMode.HardLink)
             {
-                default:
-                case FileHandlingMode.Copy:
-                    CopyFileInternal_Copy(source, destination);
-                    break;
-                case FileHandlingMode.SymbolicLink:
-                    CopyFileInternal_SymbolicLink(source, destination);
-                    break;
-                case FileHandlingMode.HardLink:
-                    CopyFileInternal_HardLink(source, destination);
-                    break;
-            }
-        }
-
-        private static void CopyFileInternal_Copy(string source, string destination)
-        {
-            File.Copy(source, destination);
-        }
-
-        private static void CopyFileInternal_SymbolicLink(string source, string destination)
-        {
-            File.CreateSymbolicLink(destination, source);
-        }
-
-        // NOTE : This makes me cry blood... runtime checks to see what OS we're on... why couldn't this shit be done at compiletime on C#??
-        // Oh how I miss macros and conditional compilation from C...
-        private static void CopyFileInternal_HardLink(string source, string destination)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                Win32Helper.CreateFileAsHardLink(source, destination);
-            }
-            else
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
-            {
-                UnixHelper.CreateFileAsHardLink(source, destination);
-            }
-            else
-            {
-                throw new NotImplementedException("Hard Link usage is not implemented yet on the current platform!)");
-            }
-        }
-
-        private static class Win32Helper
-        {
-            // Gotta love Windows and their undocumented magic numbers! took quite a while to find the definition of this macro...
-            // And ofc, here on C# it has to be a fucking const int, like wtf...
-            const int FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000;
-
-            // Windows API for creating hard links.
-            [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-            private static extern bool CreateHardLink(string lpFileName, string lpExistingFileName, nint lpSecurityAttributes);
-
-            [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-            private static extern int FormatMessage(int flags, nint source, int messageId, int languageId, StringBuilder buffer, int size, nint arguments);
-
-            private static string GetErrorMessage(int errorCode)
-            {
-                StringBuilder buffer = new StringBuilder(256);
-                FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, nint.Zero, errorCode, 0, buffer, buffer.Capacity, nint.Zero);
-                return buffer.ToString().Trim();
+                CopyFileInternal(source, destination, fileHandlingMode);
             }
 
-            public static void CreateFileAsHardLink(string origin, string destination)
+            private static void CopyFileInternal(string source, string destination, FileHandlingMode fileHandlingMode)
             {
-                bool success = CreateHardLink(destination, origin, nint.Zero);
-                if (!success)
+                switch (fileHandlingMode)
                 {
-                    int errorCode = Marshal.GetLastWin32Error();
-                    string message = GetErrorMessage(errorCode);
-                    throw new Exception(message);
+                    default:
+                    case FileHandlingMode.Copy:
+                        CopyFileInternal_Copy(source, destination);
+                        break;
+                    case FileHandlingMode.SymbolicLink:
+                        CopyFileInternal_SymbolicLink(source, destination);
+                        break;
+                    case FileHandlingMode.HardLink:
+                        CopyFileInternal_HardLink(source, destination);
+                        break;
+                }
+            }
+
+            private static void CopyFileInternal_Copy(string source, string destination)
+            {
+                File.Copy(source, destination);
+            }
+
+            private static void CopyFileInternal_SymbolicLink(string source, string destination)
+            {
+                File.CreateSymbolicLink(destination, source);
+            }
+
+            private static void CopyFileInternal_HardLink(string source, string destination)
+            {
+                // NOTE : This makes me cry blood... runtime checks to see what OS we're on... why couldn't this shit be done at compiletime on C#??
+                // Oh how I miss macros and conditional compilation from C...
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    Win32Helper.CreateFileAsHardLink(source, destination);
+                }
+                else
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+                {
+                    UnixHelper.CreateFileAsHardLink(source, destination);
+                }
+                else
+                {
+                    throw new NotImplementedException("Hard Link usage is not implemented yet on the current platform!)");
+                }
+            }
+
+            private static class Win32Helper
+            {
+                // Gotta love Windows and their undocumented magic numbers! took quite a while to find the definition of this macro...
+                // And ofc, here on C# it has to be a fucking const int, like wtf...
+                const int FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000;
+
+                // Windows API for creating hard links.
+                [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+                private static extern bool CreateHardLink(string lpFileName, string lpExistingFileName, nint lpSecurityAttributes);
+
+                [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+                private static extern int FormatMessage(int flags, nint source, int messageId, int languageId, StringBuilder buffer, int size, nint arguments);
+
+                private static string GetErrorMessage(int errorCode)
+                {
+                    StringBuilder buffer = new StringBuilder(256);
+                    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, nint.Zero, errorCode, 0, buffer, buffer.Capacity, nint.Zero);
+                    return buffer.ToString().Trim();
+                }
+
+                public static void CreateFileAsHardLink(string origin, string destination)
+                {
+                    bool success = CreateHardLink(destination, origin, nint.Zero);
+                    if (!success)
+                    {
+                        int errorCode = Marshal.GetLastWin32Error();
+                        string message = GetErrorMessage(errorCode);
+                        throw new Exception(message);
+                    }
+                }
+            }
+
+            private static class UnixHelper
+            {
+                // Linux API for creating hard links.
+                // Also works for macOS, but I think I need to use hunlink() to remove directories and files that are hard linked... not sure about that tho...
+                [DllImport("libc", SetLastError = true)]
+                private static extern int link(string oldpath, string newpath);
+
+                [DllImport("libc")]
+                private static extern nint strerror(int errnum);
+
+                private static string GetErrorMessage(int errorCode)
+                {
+                    nint messagePointer = strerror(errorCode);
+                    return Marshal.PtrToStringAnsi(messagePointer) ?? "Unknown error";
+                }
+
+                public static void CreateFileAsHardLink(string origin, string destination)
+                {
+                    int errorCode = link(origin, destination);
+                    if (errorCode != 0)
+                    {
+                        string message = GetErrorMessage(errorCode);
+                        throw new Exception(message);
+                    }
                 }
             }
         }
 
-        private static class UnixHelper
-        {
-            // Linux API for creating hard links.
-            // Also works for macOS, but I think I need to use hunlink() to remove directories and files that are hard linked... not sure about that tho...
-            [DllImport("libc", SetLastError = true)]
-            private static extern int link(string oldpath, string newpath);
-
-            [DllImport("libc")]
-            private static extern nint strerror(int errnum);
-
-            private static string GetErrorMessage(int errorCode)
-            {
-                nint messagePointer = strerror(errorCode);
-                return Marshal.PtrToStringAnsi(messagePointer) ?? "Unknown error";
-            }
-
-            public static void CreateFileAsHardLink(string origin, string destination)
-            {
-                int errorCode = link(origin, destination);
-                if (errorCode != 0)
-                {
-                    string message = GetErrorMessage(errorCode);
-                    throw new Exception(message);
-                }
-            }
-        }
+        #endregion
     }
 }
