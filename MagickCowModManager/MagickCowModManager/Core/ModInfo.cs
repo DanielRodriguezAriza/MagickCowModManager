8a69a560dc4df4c1dd11d9c6a65ac18dc00cbeaa
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-03 04:26:12 +0200

    Created ModInfo struct

---
+++ MagickCowModManager/MagickCowModManager/Core/ModInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core
+{
+    public struct ModInfo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsUnsafe { get; set; }
+        public AuthorInfo[] Authors { get; set; }
+        public string Version { get; set; }
+
+        public ModInfo()
+        {
+            this.Name = string.Empty;
+            this.Description = string.Empty;
+            this.IsUnsafe = false;
+            this.Authors = Array.Empty<AuthorInfo>();
+            this.Version = "1.0.0";
+        }
+    }
+}
