2086b16866de9b2de074da3d78ec6f94aba4f598
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-03 02:50:12 +0200

    Created file system utility static class
    This class is in charge of performing operations that are related to the file system, offering a higher level interface to abstract away the file system work and operations.

---
+++ MagickCowModManager/MagickCowModManager/Core/FileSystemUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core
+{
+    public static class FileSystemUtil
+    {
+        public static List<string> GetChildDirectories(string path)
+        {
+            return GetChildDirectories(new DirectoryInfo(path));
+        }
+
+        public static List<string> GetChildDirectories(DirectoryInfo directoryInfo)
+        {
+            var directories = directoryInfo.GetDirectories();
+            List<string> result = new List<string>(directories.Length);
+            foreach (var dir in directories)
+            {
+                result.Add(dir.Name);
+            }
+            return result;
+        }
+
+        public static List<string> GetChildFiles(string path)
+        {
+            return GetChildFiles(new DirectoryInfo(path));
+        }
+
+        public static List<string> GetChildFiles(DirectoryInfo directoryInfo)
+        {
+            var files = directoryInfo.GetFiles();
+            List<string> result = new List<string>(files.Length);
+            foreach (var file in files)
+            {
+                result.Add(file.Name);
+            }
+            return result;
+        }
+    }
+}
