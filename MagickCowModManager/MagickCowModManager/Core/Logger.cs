7e380e32532e379f392b125de39465f36a9cd84c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-03 19:44:33 +0200

    Made initial implementation of logger class

---
+++ MagickCowModManager/MagickCowModManager/Core/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core
+{
+    public struct LogMessage
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public object? data { get; set; }
+    }
+
+    public static class Logger
+    {
+        public static void Log(int status, string message, object? data = null)
+        {
+            LogMessage dataWrapper = new()
+            {
+                status = status,
+                message = message,
+                data = data
+            };
+            string json = JsonSerializer.Serialize(dataWrapper);
+            Console.WriteLine(json);
+        }
+    }
+}
