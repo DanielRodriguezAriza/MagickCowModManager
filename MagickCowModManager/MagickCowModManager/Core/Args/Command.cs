ba62510714d08b2f8ac39b0365e9bba1984011ed
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-23 17:53:12 +0200

    Created new command struct

---
+++ MagickCowModManager/MagickCowModManager/Core/Args/Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.Args
+{
+    public struct Command
+    {
+        public string ShortCommand { get; set; }
+        public string LongCommand { get; set; }
+        public string[] Arguments { get; set; }
+        public string Description { get; set; }
+        public Action<string[], int> Function { get; set; }
+
+        // Helper getter to get the Arguments as a single string
+        public string ArgumentsString
+        {
+            get
+            {
+                string argsString = "";
+                foreach (var arg in Arguments)
+                {
+                    argsString += $"<{arg}> ";
+                }
+                return argsString; // NOTE : We could trim this to remove the last space, but we don't for now. This is because the help command exploits the extra space for pretty printing.
+            }
+        }
+    }
+}
