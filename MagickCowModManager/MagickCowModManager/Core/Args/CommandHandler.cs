f4f0d1d1b36bc7f707836bebe5126f99d557c8d9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-23 17:53:52 +0200

    Created command handler class with help command
    The prupose of this class to handle command execution.
    
    TODO : Implement CmdHelp
    TODO : Implement all other commands

---
+++ MagickCowModManager/MagickCowModManager/Core/Args/CommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickCowModManager.Core.Args
+{
+    public class CommandHandler
+    {
+        public ModManager ModManager { get; set; } // Reference to a mod manager instance. Could really just be instantiated inside of this class directly, but we do it like this just in case that the ModManager class adds some statefullness in the future, which would make requiring an instance as a sort of context make sense.
+        public Command[] Commands { get; set; } // List of known commands. Could use a list in the future so that it can be modified with custom commands from the outside in case any users want to add custom commands.
+
+        public CommandHandler(ModManager modManager)
+        {
+            ModManager = modManager;
+            Commands =
+            [
+                new Command
+                {
+                    ShortCommand = "h",
+                    LongCommand = "help",
+                    Arguments = [],
+                    Description = "Display this help message",
+                    Function = CmdHelp
+                },
+            ];
+        }
+
+        public void CmdHelp(string[] args, int index)
+        {
+            // TODO : Implement
+        }
+    }
+}
