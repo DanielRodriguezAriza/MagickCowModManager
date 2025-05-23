fc9318334190be9769bf4ce61fefaffdb8b9a649
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-23 21:21:15 +0200

    Modified main to return int
    Used for process interop so that other processes that invoke this one will be capable of knowing when the result of this process was successfully executed or not.
    
    Returns 0 on success and non-zero on error.

--- MagickCowModManager/MagickCowModManager/Program.cs
+++ MagickCowModManager/MagickCowModManager/Program.cs
@@ -5,7 +5,7 @@ namespace MagickCowModManager
 {
     public class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             try
             {
@@ -17,10 +17,14 @@ namespace MagickCowModManager
 
                 modManager.ApplyProfile(args[0]);
                 Logger.Log(0, "Successfully completed the operation!");
+
+                return 0;
             }
             catch(Exception e)
             {
                 Logger.Log(1, $"An error orcurred while running the program : {e.Message}", null);
+
+                return 1;
             }
         }
     }
